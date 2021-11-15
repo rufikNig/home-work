e370d42522c019e35bd92ad5a632b78cda26c163
rufina <rufina2303@gmail.com>
2021-11-15 22:53:19 +0300

    add lombok

--- src/main/java/com/sbrf/reboot/atm/Banknote.java
+++ src/main/java/com/sbrf/reboot/atm/Banknote.java
@@ -1,6 +1,8 @@
 package com.sbrf.reboot.atm;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class Banknote{
-    public Banknote() {
-    }
+
 }
--- src/main/java/com/sbrf/reboot/atm/Cassette.java
+++ src/main/java/com/sbrf/reboot/atm/Cassette.java
@@ -1,14 +1,13 @@
 package com.sbrf.reboot.atm;
 
+import lombok.AllArgsConstructor;
+
 import java.util.ArrayList;
 
+@AllArgsConstructor
 public class Cassette <T>{
 
     private ArrayList<T> banknote;
-
-    public Cassette(ArrayList<T> banknote) {
-        this.banknote = banknote;
-    }
 
     public int getCountBanknotes() {
         return banknote.size();
